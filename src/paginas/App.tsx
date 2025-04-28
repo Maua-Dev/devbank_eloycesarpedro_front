995fdf84e3924de42adc4ba5c1fb0791094fc206
Vinicius Eloy <vinieloy@MacBook-Pro-de-Vinicius.local>
2025-04-28 16:28:10 -0300

    Validação ainda não está finalizada

--- src/paginas/App.tsx
+++ src/paginas/App.tsx
@@ -1,12 +1,31 @@
-import './App.css'
+import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  //Definindo o tipo inicial do valor inserido na caixa de texto
+  const[input, setInput] = useState<string>('');
 
   return (
     <main className='h-screen w-full flex flex-col items-center justify-center gap-4'>
       <h1>DevBank</h1>
         <div>
-          <input className = 'rounded-lg' type = "url" placeholder='Coloque o Endpoint da Sua API'/>
+          <input 
+          className = "caixaAPI" 
+          type = "string" 
+          placeholder='Coloque o Endpoint da Sua API'
+          onChange={(event) => {
+            //o algoritmo abaixo tenta transformar aquilo que foi inserido na caixa de texto
+            //em uma url, caso nao consiga, o programa vai apagar todo o conteúdo escrito na
+            //caixa de texto
+            try {
+              const url = new URL(event.target.value); // esse event.target.value é o "valor" do que for inserido na caixa do input
+              setInput(url);
+            } catch (error) {
+              // Se não for uma URL válida, mantém vazio
+              setInput(''); 
+            }
+          }}
+          />
         </div>
     </main>
   )
