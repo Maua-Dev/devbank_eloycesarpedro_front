a02137872290030c580de038c934f56ec3dad36c
veloy25 <veloy.devmaua@gmail.com>
2025-04-28 22:44:19 -0300

    Validação quase concluida

---
+++ src/paginas/App.tsx
@@ -0,0 +1,45 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  //Definindo o tipo inicial do valor inserido na caixa de texto
+  const[input, setInput] = useState<string>('');
+
+
+  //Essa função armazena o valor inserido no input no parâmetro "e"
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInput(e.target.value);
+  }
+  
+  //validação da URL da API
+  function validaURL(){
+    return input.includes("https");
+  }
+  //declaração da variável mensagemErro
+  let mensagemErro;
+
+  //esse desvio condicional serve para detectarmos se o usuário está inserindo uma URL de fato
+  if (input.length > 0 && !validaURL()) {
+    mensagemErro = <span>URL inválida</span>;
+  }
+
+  return (
+    <main className='background-color: rgb(49, 247, 0) h-screen w-full flex flex-col items-center justify-center gap-4 '>
+      <div className = "titulo"><img></img></div> 
+        <div className = "caixaAPI" >
+          <input 
+            type = "string" 
+            placeholder='Coloque o Endpoint da Sua API'
+            //a função abaixo diz para o React que essa função deve ser chamada toda vez que o input mudar
+            onChange={handleChange}
+            />
+            {mensagemErro}
+        </div>
+    </main>
+  )
+}
+  
+
+
+export default App
---
+++ src/paginas/depostirar.tsx
@@ -0,0 +1,6 @@
+import { Link } from 'react-router-dom';
+<div style={{ marginTop: 20 }}>
+<Link to="/">
+  <button>Voltar para a Página Inicial</button>
+</Link>
+</div>
