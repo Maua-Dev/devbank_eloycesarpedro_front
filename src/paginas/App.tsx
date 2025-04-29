51311684ef6fac47a5725a392f3b3d73aea760b2
veloy25 <veloy.devmaua@gmail.com>
2025-04-29 00:22:32 -0300

    Colocando a imagem do DevBank

--- src/paginas/App.tsx
+++ src/paginas/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import logo from "/imagens/logo.png";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   //Definindo o tipo inicial do valor inserido na caixa de texto
   const[input, setInput] = useState<string>('');
-
 
   //Essa função armazena o valor inserido no input no parâmetro "e"
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -21,12 +21,12 @@ function App() {
 
   //esse desvio condicional serve para detectarmos se o usuário está inserindo uma URL de fato
   if (input.length > 0 && !validaURL()) {
-    mensagemErro = <span>URL inválida</span>;
+    mensagemErro = <span className='color: rgb(255, 0, 0)'>URL inválida</span>;
   }
 
   return (
-    <main className='background-color: rgb(0, 0, 0) h-screen w-full flex flex-col items-center justify-center gap-4 '>
-      <div className = "titulo"><img></img></div> 
+    <main className='background-color: rgb(255, 0, 0) h-screen w-full flex flex-col items-center justify-center gap-4 '>
+      <div> <img src={logo} width={460} height={100} alt=""/> </div> 
         <div className = "caixaAPI" >
           <input 
             type = "string" 
