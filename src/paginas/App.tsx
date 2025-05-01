b3713c0ffcfcadd5ee569afbdb4edab869a51759
veloy25 <veloy.devmaua@gmail.com>
2025-05-01 16:11:49 -0300

    Press "Enter" validation

--- src/paginas/App.tsx
+++ src/paginas/App.tsx
@@ -1,28 +1,40 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import logo from "/imagens/logo.png";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { JSX } from 'react/jsx-runtime';
 
 function App() {
   //Definindo o tipo inicial do valor inserido na caixa de texto
   const[input, setInput] = useState<string>('');
+  const [enterPressed, setEnterPressed] = useState<0 | 1>(0);
 
   //Essa função armazena o valor inserido no input no parâmetro "e"
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setInput(e.target.value);
   }
-  
+
   //validação da URL da API
   function validaURL(){
-    return input.includes("https");
+    return input.includes("https://");
   }
+  
+  //
+  const KeyPressed = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key == 'Enter'){
+      setEnterPressed(1);
+    }else{
+      setEnterPressed(0);
+    }
+  }
+
   //declaração da variável mensagemErro
   let mensagemErro;
-
   //esse desvio condicional serve para detectarmos se o usuário está inserindo uma URL de fato
-  if (input.length > 0 && !validaURL()) {
+  if (input.length > 0 && !validaURL() && enterPressed) {
     mensagemErro = <span className='color: rgb(255, 0, 0)'>URL inválida</span>;
   }
+
 
   return (
     <main className='background-color: rgb(255, 0, 0) h-screen w-full flex flex-col items-center justify-center gap-4 '>
@@ -34,6 +46,7 @@ function App() {
             placeholder='Coloque o Endpoint da Sua API'
             //a função abaixo diz para o React que essa função deve ser chamada toda vez que o input mudar
             onChange={handleChange}
+            onKeyDown={KeyPressed}
             />
             {mensagemErro}
           </div>
